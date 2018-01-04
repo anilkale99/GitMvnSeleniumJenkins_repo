e03804ac67eb149790fdbf622c09b64fac244f60
admin <admin@DESKTOP-EKBA8NT>
2018-01-04 15:36:04 +0530

    3.0 commit

--- GitMvnSeleniumJenkins/src/test/java/com/MavenSelenium/tests/SeleniumTestScript.java
+++ GitMvnSeleniumJenkins/src/test/java/com/MavenSelenium/tests/SeleniumTestScript.java
@@ -13,19 +13,19 @@ public class SeleniumTestScript {
 	}
 	
 	
-//	@Test
-//	public void method1(){
-//		//System.setProperty("webdriver.gecko.driver", "D:\\Training\\TrainingContent\\SeleniumJars_Software\\geckodriver_64\\geckodriver.exe");
-//		
-//		WebDriver driver = new FirefoxDriver();
-//		driver.get("http://demo.automationtesting.in/Register.html");
-//		driver.manage().window().maximize();
-//		//name and address
-//		driver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys("Vinayak");
-//		driver.findElement(By.xpath("//textarea[@ng-model='Adress']")).sendKeys("Aundh");
-//		//radiobutton
-//		driver.findElement(By.xpath("//input[@value='Male']")).click();
-//		//checkbox
-//		
-//	}
+	@Test
+	public void method1(){
+		System.setProperty("webdriver.gecko.driver", "D:\\Training\\TrainingContent\\SeleniumJars_Software\\geckodriver_64\\geckodriver.exe");
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://demo.automationtesting.in/Register.html");
+		driver.manage().window().maximize();
+		//name and address
+		driver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys("Vinayak");
+		driver.findElement(By.xpath("//textarea[@ng-model='Adress']")).sendKeys("Aundh");
+		//radiobutton
+		driver.findElement(By.xpath("//input[@value='Male']")).click();
+		//checkbox
+		
+	}
 }
